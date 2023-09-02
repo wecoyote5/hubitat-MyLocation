e03634836542cbb1f1cff9dbe8f4a9e9b3ed14a1
wecoyote5 <31808973+wecoyote5@users.noreply.github.com>
2023-09-02 19:14:17 -0400

    Corrected typo

--- MyLocation.groovy
+++ MyLocation.groovy
@@ -1,7 +1,7 @@
 /*     
        Revisions
             02Sep23 - Initial Release
-            02Sep23R1 - Enhanced by @jpage4000
+            02Sep23R1 - Enhanced by @jpage4500
             	Thanks for the update
 */
  
