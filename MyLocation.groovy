9fcd54bf5544dea68474cea82944645b8a9731e6
wecoyote5 <31808973+wecoyote5@users.noreply.github.com>
2023-09-07 16:25:20 -0400

    V0.1.4

--- MyLocation.groovy
+++ MyLocation.groovy
@@ -5,6 +5,7 @@
             	Thanks for the update
             0.1.2 - 05Sep23 - Added Status and geoWKT
             0.1.3 - 06Sep23 - Shortened variables and separated accuracy
+            0.1.4 - 07Sep23 - Added name attribute, converted all key checks to use 'containsKey'
 */
  
 import groovy.json.*
@@ -27,6 +28,7 @@ metadata {
     attribute "longitude", "number"
     attribute "accuracy", "number"
     attribute "lastUpdated", "date"
+    attribute "name", "string"                          // if configured by the user, this is name of the presence zone (i.e. 'home' or 'work') the device is currently in.
 
     attribute "battery", "number"
     attribute "charging", "enum", ["true","false"]
@@ -35,7 +37,7 @@ metadata {
     attribute "status", "string"                        // what the the device is doing ("driving")
     attribute "confidence", "string"                    // how confident that status is ("75%")
     attribute "statusSet", "date"                       // last time status changed (NOTE: not always the same as lastUpdated)
-
+    
     attribute "geoWKT", "string"
 }
 
@@ -45,12 +47,16 @@ def setLocation (loc) {
     try {
         def locJson = jsonSlurper.parseText(loc)
 
-        if (locJson.lat) sendEvent(name: "latitude", value: locJson.lat)
-        if (locJson.lng) sendEvent(name: "longitude", value: locJson.lng)
-        sendEvent (name: "geoWKT", value: "POINT (${locJson.lat} ${locJson.lng})")
-
-        if (locJson.acc) sendEvent(name: "accuracy", value: locJson.acc)
-        if (locJson.bat) sendEvent(name: "battery", value: locJson.bat)
+        if (locJson.containsKey("lat")) {
+            sendEvent(name: "latitude", value: locJson.lat)
+            
+            if (locJson.containsKey("lng")) {
+                sendEvent(name: "longitude", value: locJson.lng)
+                sendEvent (name: "geoWKT", value: "POINT (${locJson.lat} ${locJson.lng})") //Only update if both lat and long were received
+            }
+        }
+        if (locJson.containsKey("acc")) sendEvent(name: "accuracy", value: locJson.acc)
+        if (locJson.containsKey("bat")) sendEvent(name: "battery", value: locJson.bat)
 
         if (locJson.containsKey("w")) sendEvent(name: "wifi", value: locJson.w == 1 ? "true" : "false")
         if (locJson.containsKey("p")) sendEvent(name: "charging", value: locJson.p == 1 ? "true" : "false")
@@ -58,6 +64,12 @@ def setLocation (loc) {
         if (locJson.containsKey("c")) sendEvent(name: "confidence", value: locJson.c + "%")
         if (locJson.containsKey("ss")) sendEvent(name: "statusSet", value: new Date(locJson.ss))
 
+        if (locJson.containsKey("l")) {
+            sendEvent(name: "name", value: locJson.l)
+        } else {
+            sendEvent(name: "name", value: null)
+        }        
+        
         sendEvent(name: "lastUpdated", value: new Date())
 
     } catch (Exception ex) {
