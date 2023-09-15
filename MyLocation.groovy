fe2cda826bd47848e20e6796ee0498a28d3f04fe
wecoyote5 <31808973+wecoyote5@users.noreply.github.com>
2023-09-14 20:44:35 -0400

    v0.1.7

--- MyLocation.groovy
+++ MyLocation.groovy
@@ -8,6 +8,9 @@
             0.1.4 - 07Sep23 - Added name attribute, converted all key checks to use 'containsKey'
             0.1.5 - 11Sep23 - Changed location name key from 'l' to 'n'
             0.1.6 - 13Sep23 - Added 'No Zone Preference' Returning 'null' did not work as intended
+            0.1.7 - 14Sep23 - Added presence function and the Presence Zone Name preference
+
+Sample for testing {"acc":14.084,"bat":63,"c":99,"lat":44.2475469,"lng":-80.1130719,"n":"at Home","p":0,"s":"still","ss":1694726682756,"w":1}
 */
  
 import groovy.json.*
@@ -17,6 +20,7 @@ metadata {
         capability "Actuator"
         capability "Battery"
         capability "Power Source"
+        capability "PresenceSensor"
 
         command('setLocation', [[name: 'Set Location', type: 'JSON_OBJECT', description: 'JSON format: {"lat":80.123, "lng":-80.123, "acc":50, "bat":10, "w":1, "p":1, "s":"driving", "c":50}']])
 
@@ -24,7 +28,8 @@ metadata {
 
     preferences {
         input name: "enlog", type: "bool", title: "Enable Logging", description: "", required: true
-        input name: "NZString", type: "string", title: "No Zone Name", description: "String Returned when not in any Zone, default is Not Present", required: true
+        input name: "NZString", type: "string", title: "No Zone Name", description: "String Returned when not in any Zone, default is Not Present", required: false
+        input name: "PresName", type: "string", title: "Presence Zone Name", description: "Zone name which sets presence. Must match zone name rec'd exactly.", required: false
     }
 
     attribute "latitude", "number"
@@ -68,15 +73,17 @@ def setLocation (loc) {
         if (locJson.containsKey("ss")) sendEvent(name: "statusSet", value: new Date(locJson.ss))
 
         if (locJson.containsKey("n")) {
-            sendEvent(name: "name", value: locJson.n)
+            locName = locJson.n
         } else {
             if (NZString != null) {
-                sendEvent(name: "name", value: NZString)
+                locName = NZString
             }else{
-                sendEvent(name: "name", value: "Not Present")
+                locName = "Not Present"
             }
         }        
-        
+        sendEvent(name: "name", value: locName)
+        sendEvent(name: "presence", value: locName == PresName ? "present" : "not present")
+
         sendEvent(name: "lastUpdated", value: new Date())
 
     } catch (Exception ex) {
